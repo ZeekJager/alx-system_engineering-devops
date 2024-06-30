c067d8e3ee6bfda76ffda4d55cc222c99314ed2b
Zekarias Tesfaye Abera <zachabera1999@gmail.com>
2024-06-30 21:24:54 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,15 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
+#install flask with puppet
+
 package { 'python3.8':
   ensure   => '3.8.10',
-  provider => 'pip3'
+  provider => 'pip3',
 }
+
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
+
 package {'werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3' 
